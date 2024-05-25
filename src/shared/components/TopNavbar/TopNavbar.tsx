358a72064a83b65916fc30163d68788279146266
zhangcheng <952490637@qq.com>
2024-05-25 20:25:22 +0800

    :bug: 引用报错

--- src/shared/components/TopNavbar/TopNavbar.tsx
+++ src/shared/components/TopNavbar/TopNavbar.tsx
@@ -1,15 +1,31 @@
-import React from 'react';
+import React, { CSSProperties } from 'react';
 import { Row, Col } from 'antd';
-import './TopNavBar.css';
 import Tabs from '@src/shared/components/Tabs/Tabs';
 
+
 const TopNavBar = () => {
+  const logoStyle: CSSProperties = {
+    width: '48px',
+    height: '48px',
+    background: 'url("/assets/png/logo.chunk.png") no-repeat center center',
+    backgroundSize: 'contain',
+    float: 'left' as CSSProperties['float'],
+    marginRight: '5px',
+  };
+
+  const titleStyle: CSSProperties = {
+    float: 'left' as CSSProperties['float'],
+    lineHeight: '48px',
+    margin: '0',
+    fontSize: '18px',
+  };
+
   return (
     <div style={{ marginLeft: '5px', marginRight: '5px' }}>
       <Row justify="space-between" align="middle">
         <Col>
-          <div className="logo" />
-          <h1 className="title">DingDuang</h1>
+          <div style={logoStyle} />
+          <h1 style={titleStyle}>DingDuang</h1>
         </Col>
         <Col>
           <Tabs />
